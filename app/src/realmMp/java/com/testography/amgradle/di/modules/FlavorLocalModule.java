1ba7fccba4a017b1dcec8a41436db0f49a2e242a
Leo Burdianov <leo.burdianov@gmail.com>
2017-01-15 23:06:21 +0400

    1:01:00

--- app/src/realmMp/java/com/testography/amgradle/di/modules/FlavorLocalModule.java
+++ app/src/realmMp/java/com/testography/amgradle/di/modules/FlavorLocalModule.java
@@ -5,11 +5,16 @@ import android.util.Log;
 
 import com.facebook.stetho.Stetho;
 import com.testography.amgradle.data.managers.RealmManager;
+import com.testography.amgradle.utils.ConstantsManager;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import io.realm.SyncCredentials;
+import io.realm.SyncUser;
+import rx.Observable;
+import rx.Subscriber;
 
 @Module
 public class FlavorLocalModule {
@@ -20,6 +25,45 @@ public class FlavorLocalModule {
     RealmManager provideRealmManager(Context context) {
         Log.e(TAG, "provideRealmManager init: ");
         Stetho.initializeWithDefaults(context);
+
+        Observable.create(new Observable.OnSubscribe<SyncUser>() {
+            @Override
+            public void call(Subscriber<? super SyncUser> subscriber) {
+                SyncCredentials syncCredentials = SyncCredentials
+                        .usernamePassword(
+                                ConstantsManager.REALM_USER,
+                                ConstantsManager.REALM_PASSWORD,
+                                false);
+                // create observer from sync result
+                if (!subscriber.isUnsubscribed()) {
+                    try {
+                        subscriber.onNext(SyncUser.login(syncCredentials,
+                                ConstantsManager.REALM_AUTH_URL));
+                        subscriber.onCompleted();
+                    } catch (Exception e) {
+                        subscriber.onError(e);
+                    }
+                }
+
+                // create observer from callback result
+                /*if (!subscriber.isUnsubscribed()) {
+                    SyncUser.loginAsync(syncCredentials, ConstantsManager
+                            .REALM_AUTH_URL, new SyncUser.Callback() {
+                        @Override
+                        public void onSuccess(SyncUser user) {
+                            subscriber.onNext(user);
+                            subscriber.onCompleted();
+                        }
+
+                        @Override
+                        public void onError(ObjectServerError error) {
+                            subscriber.onError(error);
+                        }
+                    });
+                }*/
+            }
+        });
+
         return new RealmManager();
     }
 }
--- app/src/realmMp/java/com/testography/amgradle/utils/ConstantsManager.java
+++ app/src/realmMp/java/com/testography/amgradle/utils/ConstantsManager.java
@@ -1,5 +1,7 @@
 package com.testography.amgradle.utils;
 
+import com.testography.amgradle.BuildConfig;
+
 public class ConstantsManager {
     public static final String CUSTOM_FONTS_ROOT = "fonts/";
     public static final String CUSTOM_FONT_NAME = "PTBebasNeueRegular.ttf";
@@ -16,19 +18,20 @@ public class ConstantsManager {
 
     public static final int REQUEST_PROFILE_PHOTO_PICKER = 1001;
     public static final int REQUEST_PROFILE_PHOTO_CAMERA = 1002;
-    public static final String FILE_PROVIDER_AUTHORITY = "com.testography.amgradle" +
-            ".fileprovider";
+    public static final String FILE_PROVIDER_AUTHORITY = BuildConfig
+            .APPLICATION_ID + ".fileprovider";
 
     public static final String LAST_MODIFIED_HEADER = "Last-Modified";
     public static final String IF_MODIFIED_SINCE_HEADER = "If-Modified-Since";
+    //public static final String BASE_URL = BuildConfig.HOST; define variable from build config
 
     public static final String SERVER_DATE_FORMAT = "yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'";
     public static final String TEMPORARY_USER_AVATAR =
             "http://www.topglobus.ru/forum/images/avatars/gallery/filmy/Rambo.jpg";
     public static final String TEMPORARY_USER_NAME = "Rambo";
 
-    public static final String REALM_USER = "";
-    public static final String REALM_PASSWORD = "";
-    public static final String REALM_AUTH_URL = "";
-    public static final String REALM_DB_URL = "";
+    public static final String REALM_USER = "leo@gmail.com";
+    public static final String REALM_PASSWORD = "leo";
+    public static final String REALM_AUTH_URL = "http://192.168.1.108:9080/auth";
+    public static final String REALM_DB_URL = "realm://192.168.1.108:9080/~/default";
 }
